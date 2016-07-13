e4b75bfe3f05475efa2210aaa571832e40f95fb2
Jeoygin Wang <Jeoygin Wang>
2016-07-13 09:32:57 +0800

    Add init-evil

---
+++ lisp/init-evil.el
@@ -0,0 +1,19 @@
+(require-package 'evil)
+
+(after-load 'evil
+  (setq evil-default-state 'normal)
+  (evil-mode 1)
+  (define-key evil-insert-state-map "\C-a" 'beginning-of-line)
+  (define-key evil-insert-state-map "\C-e" 'end-of-line)
+  (define-key evil-insert-state-map "\C-n" 'next-line)
+  (define-key evil-insert-state-map "\C-p" 'previous-line)
+  (define-key evil-insert-state-map "\C-v" 'scroll-up-command)
+  (define-key evil-insert-state-map "\M-v" 'scroll-down-command)
+  (define-key evil-insert-state-map "\C-k" 'kill-line)
+  (define-key evil-insert-state-map "\C-w" 'kill-region)
+  (define-key evil-insert-state-map "\C-y" 'yank)
+  )
+
+(require 'evil)
+
+(provide 'init-evil)
