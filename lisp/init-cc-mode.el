1cbe193171fc3c274332903d67f37d54cab718ba
Jeoygin Wang <Jeoygin Wang>
2016-07-13 09:34:25 +0800

    Add init-cc-mode

---
+++ lisp/init-cc-mode.el
@@ -0,0 +1,20 @@
+(nutxell/init-features '(helm company flycheck gtags ycmd))
+
+(require-package 'company-c-headers)
+
+(defun my-common-cc-mode-setup ()
+  (setq c-basic-offset 4)
+  (sanityinc/local-push-company-backend 'company-c-headers))
+
+(defun c-mode-common-hook-setup ()
+  (my-common-cc-mode-setup)
+  (company-mode t)
+  (flycheck-mode t)
+  (helm-mode t)
+  (helm-gtags-mode t)
+  (ycmd-mode t)
+  )
+
+(add-hook 'c-mode-common-hook 'c-mode-common-hook-setup)
+
+(provide 'init-cc-mode)
