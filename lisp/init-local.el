889caa9031d4e04816232abb7905319d9b9c0470
Jeoygin Wang <jeoygin@gmail.com>
2016-07-13 21:12:13 +0800

    Update init-local

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,6 +1,10 @@
-(set-variable 'ycmd-server-command '("python" "/Users/jeoygin/projects/ycmd/ycmd/"))
+(setq ycmd-server-command '("python" "/Users/jeoygin/projects/ycmd/ycmd/"))
 
 (setq tramp-ssh-controlmaster-options
     "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+
+(setq ad-redefinition-action 'accept)
+
+(diminish 'abbrev-mode)
 
 (provide 'init-local)
