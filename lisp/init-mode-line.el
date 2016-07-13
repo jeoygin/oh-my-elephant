a5ac872cb48041209d8ce2f24efff3939654d6d2
Jeoygin Wang <jeoygin@gmail.com>
2016-07-13 21:11:55 +0800

    Add init mode line

---
+++ lisp/init-mode-line.el
@@ -0,0 +1,9 @@
+(when (maybe-require-package 'smart-mode-line)
+  (require 'smart-mode-line)
+  (setq sml/no-confirm-load-theme t)
+  (setq sml/theme 'respectful)
+  (setq sml/mode-width 0)
+  (setq sml/name-width 20)
+  (sml/setup))
+
+(provide 'init-mode-line)
