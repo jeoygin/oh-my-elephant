6bab47eee2b36b0fa0615284a6a3c7411b1ce8ee
Jeoygin Wang <Jeoygin Wang>
2016-07-13 09:32:23 +0800

    Fix init-xterm

--- lisp/init-xterm.el
+++ lisp/init-xterm.el
@@ -11,7 +11,15 @@
     (define-key map "\e[5A"   [C-up])
     (define-key map "\e[5B"   [C-down])
     (define-key map "\e[5C"   [C-right])
-    (define-key map "\e[5D"   [C-left])))
+    (define-key map "\e[5D"   [C-left])
+    (define-key map "\e[1;9A" [M-up])
+    (define-key map "\e[1;9B" [M-down])
+    (define-key map "\e[1;9C" [M-right])
+    (define-key map "\e[1;9D" [M-left])
+    (define-key map "\e[1;10A" [M-S-up])
+    (define-key map "\e[1;10B" [M-S-down])
+    (define-key map "\e[1;10C" [M-S-right])
+    (define-key map "\e[1;10D" [M-S-left])))
 
 (global-set-key [mouse-4] (lambda () (interactive) (scroll-down 1)))
 (global-set-key [mouse-5] (lambda () (interactive) (scroll-up 1)))
@@ -19,11 +27,11 @@
 (defun sanityinc/console-frame-setup ()
   (when (< emacs-major-version 23)
     (fix-up-xterm-control-arrows))
+  (unless window-system
+    (fix-up-xterm-control-arrows))
   (xterm-mouse-mode 1) ; Mouse in a terminal (Use shift to paste with middle button)
   (when (fboundp 'mwheel-install)
     (mwheel-install)))
-
-
 
 (add-hook 'after-make-console-frame-hooks 'sanityinc/console-frame-setup)
 
