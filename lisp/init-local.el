db8f011c80015ad53cb41c2572d1fede18a051da
Jeoygin Wang <jeoygin@gmail.com>
2017-11-11 22:11:45 -0800

    Update org config

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -18,8 +18,9 @@
 ;; customize-group org-export
 ;; (setq org-export-backends '(ascii beamer html icalendar latex odt))
 ;; customize-variable org-modules
-;; (setq org-modules '(org-bbdb org-bibtex org-docview org-gnus org-habit org-id org-info org-irc org-mhe org-rmail org-w3m))
+;; (setq org-modules '(org-bbdb org-bibtex org-docview org-gnus org-habit org-id org-info org-irc org-mhe org-rmail org-w3m org-checklist))
 
+(setq org-startup-indented t)
 (setq org-clock-history-length 25)
 
 (setq org-default-notes-file (concat org-directory "/notes.org"))
@@ -29,7 +30,7 @@
 (setq org-capture-journal-file (concat org-directory "/journal/" (format-time-string "%Y") ".org"))
 (setq org-capture-work-journal-file (concat org-directory "/journal/w" (format-time-string "%Y") ".org"))
 
-(setq org-id-link-to-org-use-id 'use-existing)
+(setq org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
 (setq org-id-locations-file "~/Dropbox/org/org-id-locations")
 
 (setq org-tag-alist '((:startgroup . nil)
@@ -51,25 +52,27 @@
 
 (setq org-capture-templates
       `(("t" "todo" entry (file+headline "" "Tasks")  ; "" => org-default-notes-file
-         "* TODO %?\n  :LOGBOOK:\n  - Added on %U\n  :END:\n  %i\n  %a\n" :clock-resume t)
+         "* TODO %?\n:LOGBOOK:\n- Added on %U\n:END:\n%i\n%a\n" :clock-resume t)
         ("r" "respond" entry (file+headline "" "Mails")
-         "* NEXT Respond to %^{From} on %^{Subject}\n  SCHEDULED: %t\n  :LOGBOOK:\n  - Added on %U\n  :END:\n  %a\n" :clock-in t :clock-resume t)
+         "* NEXT Respond to %^{From} on %^{Subject}\nSCHEDULED: %t\n:LOGBOOK:\n- Added on %U\n:END:\n%a\n" :clock-in t :clock-resume t)
         ("m" "meeting" entry (file+headline "" "Meetings")
-         "* MEETING with %? :meeting:\n  :LOGBOOK:\n  - Added on %U\n  :END:\n" :clock-in t :clock-keep t)
+         "* MEETING %? :meeting:\n%U" :clock-in t :clock-keep t)
+        ("p" "phone call" entry (file+headline "" "Phone Calls")
+         "* PHONE with %? :phone:\n%U" :clock-in t :clock-resume t)
         ("n" "note" entry (file+headline "" "Notes")
-         "* %? :NOTE:\n  %i\n  %a\n" :clock-resume t)
+         "* %? :NOTE:\n%i\n%a\n" :clock-resume t)
         ("s" "subtask" entry (clock)
-         "* TODO %?\n  :LOGBOOK:\n  - Added on %U\n  :END:\n  %i\n  %a\n" :clock-resume t)
+         "* TODO %?\n:LOGBOOK:\n- Added on %U\n:END:\n%i\n%a\n" :clock-resume t)
         ("b" "Book" entry (file+headline org-capture-book-file "Books")
-         "* TODO %^{Author} - %^{Title}\n  :PROPERTIES:\n  :PAGES: %^{Pages}\n  :GENRE: %^{Genre}\n  :END:\n  :LOGBOOK:\n  - Added: %U\n  :END:\n  -Recommended by %^{Recommended by}\n  %i\n" :clock-resume t)
+         "* TODO %^{Author} - %^{Title}\n:PROPERTIES:\n:PAGES: %^{Pages}\n:GENRE: %^{Genre}\n:END:\n:LOGBOOK:\n- Added: %U\n:END:\n-Recommended by %^{Recommended by}\n%i\n" :clock-resume t)
         ("I" "Interview" entry (file+headline org-capture-interview-file "Interviews")
-         "* TODO %?\n    :LOGBOOK:\n    - Added on %U\n    :END:\n    :INFO:\n    :Location: %^{Location}\n    :Interviewer: %^{Interviewer}\n    :Interviewee: %^{Interviewee}\n    :Position: %^{Position}\n    :END:\n  %i\n  %a\n" :clock-resume t)
+         "* TODO %?\n:LOGBOOK:\n- Added on %U\n:END:\n:INFO:\n:Location: %^{Location}\n:Interviewer: %^{Interviewer}\n:Interviewee: %^{Interviewee}\n:Position: %^{Position}\n:END:\n%i\n%a\n" :clock-resume t)
         ("i" "Issue" entry (file+headline org-capture-issue-file "Issues")
-         "* TODO %^{ISSUE_ID}: %?\n  :PROPERTIES:\n  :CUSTOM_ID: %\\1\n  :URL: %^{URL}/%\\1\n  :END:\n  :LOGBOOK:\n  - Added on %U\n  :END:\n  %i\n" :clock-resume t)
+         "* TODO %^{ISSUE_ID}: %?\n:PROPERTIES:\n:CUSTOM_ID: %\\1\n:URL: %^{URL}/%\\1\n:END:\n:LOGBOOK:\n- Added on %U\n:END:\n%i\n" :clock-resume t)
         ("j" "Journal" entry (file+datetree+prompt org-capture-journal-file)
-         "* %? [%^{duration|0} MIN]\n  :LOGBOOK:\n  - Added on [%<%Y-%m-%d %H:%M %a>]\n  :END:\n  %i\n  %a\n" :clock-resume t)
+         "* %? [%^{duration|0} MIN]\n:LOGBOOK:\n- Added on [%<%Y-%m-%d %H:%M %a>]\n:END:\n%i\n%a\n" :clock-resume t)
         ("w" "Work Journal" entry (file+datetree org-capture-work-journal-file)
-         "* %? [%^{duration|0} MIN]\n  :LOGBOOK:\n  - Added on [%<%Y-%m-%d %H:%M %a>]\n  :END:\n  %i\n  %a\n" :clock-in t :clock-resume t)
+         "* %? [%^{duration|0} MIN]\n:LOGBOOK:\n- Added on [%<%Y-%m-%d %H:%M %a>]\n:END:\n%i\n%a\n" :clock-in t :clock-resume t)
         ))
 
 (add-to-list 'auto-mode-alist '("\\.page\\'" . org-mode))
